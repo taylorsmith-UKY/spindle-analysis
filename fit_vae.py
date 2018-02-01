886842c6137301910b3c56bb4a01c11555fa060f
Taylor <taylor.smith2@uky.edu>
2018-02-01 10:07:03 -0500

    update-fit-driver
    fit_vae will now read the configuration file to determine whether to use a traditional vae or the convolutional vae. Additionally, it will check to see if there is already a model saved at the name in the config file and if so it will load that model and then save additional training to a new file with '_copy' as a suffix in the filename. Also no longer wastefully loads the HDF data file just to check the dimensions, since those are in the config file as well

--- fit_vae.py
+++ fit_vae.py
@@ -9,7 +9,8 @@ Created on Tue Jan  2 15:42:12 2018
 from spindle_ae import get_vae, get_conv_vae
 from keras.callbacks import ModelCheckpoint
 from keras.utils.io_utils import HDF5Matrix
-import h5py
+from keras.models import load_model
+import numpy as np
 import json
 import os
 
@@ -25,23 +26,29 @@ vae_loc=conf['vae_loc']
 netFilename=conf['netFilename']
 gpu_id=conf['gpu_id']
 n_epochs=conf['n_epochs']
+vae_type=conf['vae_type']
+orig_dim=conf['original_dim']
+batch_size=conf['batch_size']
 
-ds_name = 'spectrograms_'+str(f_crop[0])+'-'+str(f_crop[1])+'_rsz_norm_concat_flat'
-#ds_name = 'spectrograms_'+str(f_crop[0])+'-'+str(f_crop[1])+'_rsz_norm_concat'
 dataFile = output_path + 'spindles.h5'
 vae_name = vae_loc + netFilename
 
 os.environ["CUDA_VISIBLE_DEVICES"]=gpu_id
 
-f=h5py.File(dataFile,'r')
-rds=f[ds_name]
-l = rds.shape[1]
-vae,vae_loss = get_vae(l)
-#vae,vae_loss = get_vae((299,299,3))
-f.close()
+if vae_type[:4].lower() == 'trad':
+    ds_name = 'spectrograms_'+str(f_crop[0])+'-'+str(f_crop[1])+'_rsz_norm_concat_flat'
+    l=np.prod(orig_dim)
+    vae,vae_loss = get_vae(l)
+elif vae_type[:4].lower() == 'conv':
+    ds_name = 'spectrograms_'+str(f_crop[0])+'-'+str(f_crop[1])+'_rsz_norm_concat'
+    vae,vae_loss = get_conv_vae(orig_dim)
 
 rds = HDF5Matrix(dataFile,ds_name)
 
+if os.path.exists(vae_name):
+    vae = load_model(vae_name, custom_objects={'latent_dim': latent_dim, 'epsln_std': epsln_std, 'vae_loss': vae_loss})
+    vae_name = vae_name.split('.')[0]+'_copy.h5'
+
 model_checkpoint = ModelCheckpoint(vae_name, monitor='loss',verbose=1, save_best_only=True)
 
-vae.fit(rds, rds, epochs=n_epochs, verbose=1, validation_split=0.2, shuffle=True, callbacks=[model_checkpoint]))
+vae.fit(rds, rds, batch_size=batch_size, epochs=n_epochs, verbose=1, validation_split=0.2, shuffle='batch', callbacks=[model_checkpoint])
